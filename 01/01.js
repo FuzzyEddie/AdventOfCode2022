5d75fa7066561b00ea1e5752509986c0cb110006
FuzzyEddie <eddiemcfuzz@gmail.com>
2022-12-02 15:58:56 -0600

    Question 2

--- 01/01.js
+++ 01/01.js
@@ -2,7 +2,30 @@
 //Take all elves, add their calories and add them to a map
 //Sort the map by highest to lowest
 //return the elf carrying highest calories
-exports.__esModule = true;
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
+Object.defineProperty(exports, "__esModule", { value: true });
 //or
 //Take all elves, add their calories and add them to an array
 //Loop through the array to find the index of the highest calorie elf
@@ -10,34 +33,68 @@ exports.__esModule = true;
 //Go through the list counting elves as you go
 //Add the elf's calories. If it's the highest you've seen, log that count
 //Logged count at the end is the highest calorie elf
-var fs = require("fs");
+const fs = __importStar(require("fs"));
+const filePath = "C:/CodingProjects/AdventOfCode2022/01/input.txt";
 function getGreedyElf(file) {
-    var input = fs.readFileSync(file, "utf8");
-    var greediestElf = {
+    const input = fs.readFileSync(file, "utf8");
+    const greediestElf = {
         index: 0,
         calories: 0
     };
-    var index = 0;
-    var elfIndex = 1;
-    var calCount = [];
-    while (index < input.length) {
-        var numStart = index;
-        var numEnd = input.indexOf("\n");
-        var num = parseInt(input.slice(numStart, numEnd));
+    let index = 0;
+    let elfIndex = 1;
+    let calCount = [];
+    do {
+        let numStart = index;
+        let numEnd = input.indexOf("\n", index);
+        let num = parseInt(input.slice(numStart, numEnd));
         if (!isNaN(num)) {
             calCount.push(parseInt(input.slice(numStart, numEnd)));
         }
         else {
-            var calTotal = calCount.reduce(function (val1, val2) { return val1 + val2; });
+            let calTotal = calCount.reduce((val1, val2) => val1 + val2);
+            console.log("Elf " + elfIndex + " has " + calTotal + " calories!");
             if (calTotal > greediestElf.calories) {
                 greediestElf.index = elfIndex;
                 greediestElf.calories = calTotal;
             }
-            console.log("Elf " + elfIndex + " has " + calTotal + " calories!");
+            calCount = [];
             elfIndex++;
         }
         index = numEnd + 1;
-    }
-    return greediestElf.index;
+    } while (index < input.length);
+    return greediestElf;
 }
-console.log(getGreedyElf("C:/CodingProjects/AdventOfCode2022/01/testInput.txt"));
+function topThreeElves(file) {
+    const input = fs.readFileSync(file, "utf8");
+    let topThree = [0, 0, 0];
+    let index = 0;
+    let elfIndex = 1;
+    let calCount = [];
+    do {
+        let numStart = index;
+        let numEnd = input.indexOf("\n", index);
+        let num = parseInt(input.slice(numStart, numEnd));
+        if (!isNaN(num)) {
+            calCount.push(parseInt(input.slice(numStart, numEnd)));
+        }
+        else {
+            let calTotal = calCount.reduce((val1, val2) => val1 + val2);
+            console.log("Elf " + elfIndex + " has " + calTotal + " calories!", elfIndex);
+            for (let x = 0; x < 3; x++) {
+                if (calTotal > topThree[x]) {
+                    topThree[x] = calTotal;
+                    topThree.sort();
+                    break;
+                }
+            }
+            calCount = [];
+            elfIndex++;
+        }
+        index = numEnd + 1;
+    } while (index < input.length);
+    return topThree;
+}
+// let greediestElf = getGreedyElf(filePath)
+let topThreeTotal = topThreeElves(filePath).reduce((val1, val2) => val1 + val2);
+console.log("Top three elves have", topThreeTotal);
--- 01/01.ts
+++ 01/01.ts
@@ -14,10 +14,17 @@
 //Logged count at the end is the highest calorie elf
 import * as fs from "fs"
 
-function getGreedyElf(file: string): number
+type Elf = {
+    index: number,
+    calories: number
+}
+
+const filePath = "C:/CodingProjects/AdventOfCode2022/01/input.txt"
+
+function getGreedyElf(file: string): Elf
 {
     const input = fs.readFileSync(file, "utf8")
-    const greediestElf =
+    const greediestElf: Elf =
     {
         index: 0,
         calories: 0
@@ -26,12 +33,12 @@ function getGreedyElf(file: string): number
     let elfIndex = 1
     let calCount: number[] = []
 
-    while(index < input.length)
+    do
     {
         let numStart = index
-        let numEnd = input.indexOf("\n")
+        let numEnd = input.indexOf("\n", index)
         let num = parseInt(input.slice(numStart, numEnd))
-
+        
         if (!isNaN(num))
         {
             calCount.push(parseInt(input.slice(numStart, numEnd)))
@@ -39,20 +46,67 @@ function getGreedyElf(file: string): number
         else
         {
             let calTotal = calCount.reduce((val1, val2) => val1 + val2)
+            console.log("Elf " + elfIndex + " has " + calTotal + " calories!")
             if (calTotal > greediestElf.calories) {
                 greediestElf.index = elfIndex
                 greediestElf.calories = calTotal
             }
-            console.log("Elf " + elfIndex + " has " + calTotal + " calories!")
+            calCount = []
             elfIndex++
         }
-
+        
         index = numEnd + 1
-
+        
     }
+    while(index < input.length)
     
-    return greediestElf.index
-
+    return greediestElf
 }
 
-console.log(getGreedyElf("C:/CodingProjects/AdventOfCode2022/01/testInput.txt"))
+function topThreeElves(file: string): number[]
+{
+    const input = fs.readFileSync(file, "utf8")
+    let topThree: number[] = [0, 0, 0]
+    let index = 0
+    let elfIndex = 1
+    let calCount: number[] = []
+
+    do
+    {
+        let numStart = index
+        let numEnd = input.indexOf("\n", index)
+        let num = parseInt(input.slice(numStart, numEnd))
+        
+        if (!isNaN(num))
+        {
+            calCount.push(parseInt(input.slice(numStart, numEnd)))
+        }
+        else
+        {
+            let calTotal = calCount.reduce((val1, val2) => val1 + val2)
+            console.log("Elf " + elfIndex + " has " + calTotal + " calories!", elfIndex)
+            for (let x = 0; x < 3; x++)
+            {
+                if (calTotal > topThree[x])
+                {
+                    topThree[x] = calTotal
+                    topThree.sort()
+                    break
+                }
+            }
+
+            calCount = []
+            elfIndex++
+        }
+        
+        index = numEnd + 1
+        
+    }
+    while(index < input.length)
+    
+    return topThree
+}
+
+// let greediestElf = getGreedyElf(filePath)
+let topThreeTotal = topThreeElves(filePath).reduce((val1, val2) => val1 + val2) 
+console.log("Top three elves have", topThreeTotal)
