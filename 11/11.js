95886b9ff2e7d214c94d3d365328566fc463e05e
FuzzyEddie <eddiemcfuzz@gmail.com>
2022-12-11 15:33:08 -0600

    Day 11 compiled

--- 11/11.js
+++ 11/11.js
@@ -5,7 +5,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const fs_1 = __importDefault(require("fs"));
 const path_1 = __importDefault(require("path"));
-const input = fs_1.default.readFileSync(path_1.default.resolve(__dirname, "./testinput.txt"), "utf8").split("\r\n");
+const input = fs_1.default.readFileSync(path_1.default.resolve(__dirname, "./input.txt"), "utf8").split("\r\n");
 class Monkey {
     /*
     Arguments provided as an array of 5 strings:
@@ -19,22 +19,19 @@ class Monkey {
         this.mBuis = 0;
         //Parsing starting items
         let items = input[0].replace("Starting items: ", "").split(", ");
-        this.items = items.map((x) => BigInt(parseInt(x)));
+        this.items = items.map((x) => parseInt(x));
         //Building inspect method
-        let opStr = input[1].replace("Operation: new = old ", "").split(" ");
-        if (opStr[1] !== "old") {
-            opStr[1] = "BigInt(" + opStr[1] + ")";
-        }
+        let opStr = input[1].replace("Operation: new = ", "");
         this.inspect = new Function("old", `
       this.mBuis++
-      this.items[0] = old ${opStr[0]} ${opStr[1]}`);
+      this.items[0] = ${opStr}`);
         //Building throw method
-        let test = BigInt(parseInt(input[2].replace("Test: divisible by ", "")));
+        let test = parseInt(input[2].replace("Test: divisible by ", ""));
         let ifTrue = parseInt(input[3].replace("If true: throw to monkey ", ""));
         let ifFalse = parseInt(input[4].replace("If false: throw to monkey ", ""));
+        this.divisor = test;
         this.throwTo = new Function(`
       let item = this.items[0]
-      console.log(this.items[0], "${test}", item % ${test})
       if (item % ${test} == 0)
       {
         return ${ifTrue}
@@ -57,22 +54,27 @@ class Troop {
     }
 }
 let troop = new Troop(input);
+let primeProd = 1;
+troop.monkeys.forEach((a) => primeProd *= a.divisor);
 const rounds = 10000;
 const knownRounds = [1, 20, 1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000];
 for (let t = 0; t < rounds; t++) {
-    if (knownRounds.includes(t)) {
-        debugger;
-    }
+    // if (knownRounds.includes(t))
+    // {
+    //   debugger
+    // }
     troop.monkeys.forEach((monkey) => {
         while (monkey.items.length > 0) {
             monkey.inspect(monkey.items[0]);
             // monkey.items[0] = Math.floor(monkey.items[0] / 3)
+            monkey.items[0] = monkey.items[0] % primeProd;
             troop.monkeys[monkey.throwTo()].items.push(monkey.items[0]);
             monkey.items.shift();
         }
     });
 }
-// troop.monkeys.sort((a, b) => b.mBuis - a.mBuis)
-// let totalMbuis = troop.monkeys[0].mBuis * troop.monkeys[1].mBuis
+troop.monkeys.sort((a, b) => b.mBuis - a.mBuis);
+let totalMbuis = troop.monkeys[0].mBuis * troop.monkeys[1].mBuis;
+console.log(totalMbuis);
 debugger;
 //# sourceMappingURL=11.js.map
